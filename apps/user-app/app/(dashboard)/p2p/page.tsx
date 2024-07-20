eda569a27d10fa7b62d677865f6fe4e6ca9fbd5f
Arjit <arjitchitkara004@gmail.com>
2024-07-20 17:48:21 +0530

    updated db and added p2p transfers

---
+++ apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -0,0 +1,9 @@
+import { SendCard } from "../../../components/SendCard";
+
+export default function P2PTransfer() {
+  return (
+    <div className="w-full">
+      <SendCard />
+    </div>
+  );
+}
